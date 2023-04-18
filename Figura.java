a3d060e9efbd4c83944719cf8735f726d77eb0ab
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-04-18 14:41:32 +0000

    salvar

---
+++ Figura.java
@@ -0,0 +1,5 @@
+abstract class Figura{
+    protected String cor;
+    public abstract double area();
+    
+}
---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        Figura figura1 = new Circulo("Vermelho", 5);
+        Figura figura2 = new Retangulo("roxo", 3, 4);
+
+        System.out.println("A área do círculo é: " + figura1.area() + ", a cor do círculo é: " + figura1.cor);
+        System.out.println("A área do retângulo é: " + figura2.area() + ", a cor do retângulo é:  " + figura2.cor);
+    }
+}
